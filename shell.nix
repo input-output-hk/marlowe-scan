4d307ec48178961896f163bf7eec1991c569e219
Pablo Lamela <pablo.lamela@iohk.io>
2023-02-01 16:39:43 +0100

    test - please ignore

--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@ let
   f = { mkDerivation, base, blaze-html, gtk, lib, process
       , temporary, zlib, aeson, servant-server, wai, warp, haskell-language-server
       , hspec, hspec-wai, servant-blaze, http-client, wl-pprint
-      , http-conduit, http-types
+      , http-conduit, http-types, cmdargs
       }:
       mkDerivation {
         pname = "marlowe-explorer";
@@ -16,7 +16,7 @@ let
         executableHaskellDepends = [
           base blaze-html gtk process temporary zlib
           aeson servant-server wai warp
-          haskell-language-server 
+          haskell-language-server cmdargs
           hspec hspec-wai servant-blaze
           http-client wl-pprint http-conduit http-types
         ];
