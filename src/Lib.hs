56efbde3beb334d8f307496e6aea18a2ea1c3130
Dino Morelli <dino.morelli@iohk.io>
2023-02-15 14:50:02 -0500

    Reformatted the web server code
    Changed spacing and indenting, sometimes using more whitespaces.
    Reordered the REST endpoints to be more logically like how they would be
    called during use of the app.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,21 +1,22 @@
-{-# LANGUAGE DataKinds       #-}
-{-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Lib
-    ( startApp
-    , app
-    ) where
+  ( startApp
+  , app
+  ) where
 
-import Control.Monad.IO.Class (liftIO)
-import Control.Newtype.Generics (op)
+import Control.Monad.IO.Class ( liftIO )
+import Control.Newtype.Generics ( op )
 import Network.Wai ( Application )
 import Network.Wai.Handler.Warp ( run )
 import Servant ( serve, Proxy(..), type (:>), Get, type (:<|>) ((:<|>)), QueryParam )
 import Servant.HTML.Blaze ( HTML )
 import Servant.Server ( hoistServer )
 
-import Explorer.Web.ContractView (ContractView (..), contractView)
-import Opts (Options (optExplorerPort), ExplorerPort (..))
+import Explorer.Web.ContractView ( ContractView (..), contractView )
+import Opts ( Options (optExplorerPort), ExplorerPort (..) )
+
 
 startApp :: Options -> IO ()
 startApp opts = do
@@ -24,11 +25,14 @@ startApp opts = do
     <> show eport
   run eport $ app opts
 
-type API = "contractView" :> QueryParam "tab" String :> QueryParam "contractId" String :> Get '[HTML] ContractView
-      :<|> "listContracts" :> Get '[HTML] String
+type API
+     = "listContracts" :> Get '[HTML] String
+  :<|> "contractView" :> QueryParam "tab" String :> QueryParam "contractId" String :> Get '[HTML] ContractView
 
 app :: Options -> Application
-app opts = serve (Proxy :: Proxy API) (hoistServer (Proxy :: Proxy API) liftIO (contractView opts :<|> listContracts))
+app opts = serve (Proxy :: Proxy API) $ hoistServer (Proxy :: Proxy API) liftIO $
+       listContracts
+  :<|> contractView opts
 
 listContracts :: IO String
 listContracts = return "Under contruction!"
